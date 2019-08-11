b9d34d90399e49802c8deee02e4f1ec8c25e4453
msaenzg161 <msaenzg161@gmail.com>
2019-08-11 15:58:32 -0600

    Pantallas cliente, revisar html login

---
+++ H-Mandiola/H-Mandiola/Controllers/ClienteController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace H_Mandiola.Controllers
+{
+    public class ClienteController : Controller
+    {
+
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        public ActionResult CrearCliente()
+        {
+            return View();
+        }
+
+        public ActionResult Reserva()
+        {
+            return View();
+        }
+
+        public ActionResult CambiarPass()
+        {
+            return View();
+        }
+
+        public ActionResult CatalogoHab()
+        {
+            return View();
+        }
+    }
+}
